d0f33f03322e8accda2d0b3c581279cf2a2c5bb0
mohammad <mohammadadnanabualasal>
2022-09-24 19:56:55 +0300

    Initial commit

---
+++ src/main/java/com/example/exams/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.exams;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExamsApplication.class);
+    }
+
+}
